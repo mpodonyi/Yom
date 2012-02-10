f9dead01958da009df2dbd76611bfcdb88040d90
Mike Podonyi <mikepodonyi@gmail.com>
2012-02-10 09:22:38 +0000

    Test

---
+++ src/Yom.Web/Test.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Yom.Web
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string TestValue
+        {
+            get {
+                return System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            }
+        }
+    }
+}
